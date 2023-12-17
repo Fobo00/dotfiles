6ab914712fd395758cfefe07be460c3e22fcc8a2
Fobo00 <francesco.boscain@gmail.com>
2023-12-17 22:07:55 +0100

    x

--- lua/cmp-conf.lua
+++ lua/cmp-conf.lua
@@ -162,4 +162,16 @@ cmp.event:on(
 	})
 )
 
+vim.lsp.util.stylize_markdown = function(bufnr, contents, opts)
+	contents = vim.lsp.util._normalize_markdown(contents, {
+		width = vim.lsp.util._make_floating_popup_size(contents, opts),
+	})
+
+	vim.bo[bufnr].filetype = "markdown"
+	vim.treesitter.start(bufnr)
+	vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
+
+	return contents
+end
+
 -- require('crates').setup {}
