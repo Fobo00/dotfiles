6e0057ced9fb2e25e7d98cf9ebaca6fc1a83afda
Fobo00 <francesco.boscain@gmail.com>
2025-09-15 10:17:33 +0200

    Bufferline?

--- lua/bufferline-conf.lua
+++ lua/bufferline-conf.lua
@@ -9,7 +9,7 @@ bufferline.setup({
         options = {
             mode = "buffers", -- set to "tabs" to only show tabpages instead
             style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
-            themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
+            themable = false, -- allows highlight groups to be overriden i.e. sets highlights as defaultbuff
             numbers = "none",
             close_command = "bdelete! %d",       -- can be a string | function, | false see "Mouse actions"
             right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
