cb45ce9aab5c0369e3d42d4a9ef47fca4ec1941a
Fobo00 <francesco.boscain@gmail.com>
2025-09-15 10:09:09 +0200

    Fixed DAP (Work in progress)

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,20 +1,23 @@
 return {
 
 	{
+		enabled = true,
+		lazy = true,
 		"jay-babu/mason-nvim-dap.nvim",
 		requires = "mason.nvim",
-		config = function()
-			require("mason-nvim-dap").setup {}
-		end
+		opts = {},
 	},
 
 	{
+		enabled = true,
 		"mfussenegger/nvim-dap",
-		-- requires = "rcarriga/nvim-dap-ui",
 		keys = {
-			{ "<leader><C-b>",   ":lua require'dap'.toggle_breakpoint() <CR>", "n", { silent = true } },
-			{ "<leader>dbg", ":lua require'dap'.continue() <CR>",          "n", { silent = true } },
-			{ "<leader>dui", ":lua require'dap'.repl.open() <CR>",         "n", { silent = true } },
+			{ "<leader><C-b>", ":lua require'dap'.toggle_breakpoint() <CR>", "n", { silent = true } },
+			{ "<leader>dbg",   ":lua require'dap'.continue() <CR>",          "n", { silent = true } },
+			{ "<leader>dui",   ":lua require'dap'.repl.open() <CR>",         "n", { silent = true } },
+		},
+		dependencies = {
+			-- { "igorlfs/nvim-dap-view", opts = {} },
 		},
 		config = function()
 			local dap = require("dap")
@@ -39,46 +42,46 @@ return {
 			-- dap.configurations.cpp = dap.configurations.c
 
 			-- CODELLDB
-			dap.adapters.codelldb = {
-				type = 'server',
-				host = 'localhost',
-				port = "${port}",
-				executable = {
-					-- CHANGE THIS to your path!
-					command = vim.fn.stdpath("data") .. "/mason/packages\\codelldb\\extension\\adapter\\codelldb.exe",
-					args = { "--port", "${port}" },
-
-					-- On windows you may have to uncomment this:
-					detached = false,
-				}
-			}
-
-			dap.configurations.cpp = {
-				{
-					name = "Launch file",
-					type = "codelldb",
-					request = "launch",
-					program = function()
-						return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-					end,
-					cwd = '${workspaceFolder}',
-					stopOnEntry = true,
-				},
-				{
-					name = "Attach to process",
-					type = "codelldb",
-					request = "attach",
-					processId = require("dap.utils").pick_process,
-					cwd = "${workspaceFolder}",
-				}
-			}
-			dap.configurations.c = dap.configurations.cpp
+			-- dap.adapters.codelldb = {
+			-- 	type = 'server',
+			-- 	host = 'localhost',
+			-- 	port = "${port}",
+			-- 	executable = {
+			-- 		-- CHANGE THIS to your path!
+			-- 		command = vim.fn.stdpath("data") .. "/mason/packages\\codelldb\\extension\\adapter\\codelldb.exe",
+			-- 		args = { "--port", "${port}" },
+			--
+			-- 		-- On windows you may have to uncomment this:
+			-- 		detached = false,
+			-- 	}
+			-- }
+			--
+			-- dap.configurations.cpp = {
+			-- 	{
+			-- 		name = "Launch file",
+			-- 		type = "codelldb",
+			-- 		request = "launch",
+			-- 		program = function()
+			-- 			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+			-- 		end,
+			-- 		cwd = '${workspaceFolder}',
+			-- 		stopOnEntry = true,
+			-- 	},
+			-- 	{
+			-- 		name = "Attach to process",
+			-- 		type = "codelldb",
+			-- 		request = "attach",
+			-- 		processId = require("dap.utils").pick_process,
+			-- 		cwd = "${workspaceFolder}",
+			-- 	}
+			-- }
+			-- dap.configurations.c = dap.configurations.cpp
 		end
 	},
 
 	{
-		"rcarriga/nvim-dap-ui",
 		enabled = false,
+		"rcarriga/nvim-dap-ui",
 		requires = "mfussenegger/nvim-dap",
 		config = function()
 			require("dapui").setup()
